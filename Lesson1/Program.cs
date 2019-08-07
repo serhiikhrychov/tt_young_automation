5ae5cb968bc1bb6accf26334c5e92b8bafbf252a
Serhii <sergeyhall@gmail.com>
2019-08-07 18:16:35 +0300

    Add files via upload

---
+++ Lesson1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Nice to meet you {name}");
+            Console.WriteLine("How old are you?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Ok {age} is allowed on this site!");
+            Console.ReadKey();
+        }
+    }
+}
